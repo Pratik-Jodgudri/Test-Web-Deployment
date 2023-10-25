b9c4757f9edcf3d3616a1d5825d2d8fa6783ed89
Pratik-Jodgudri <pratikjodpj@gmail.com>
2023-10-25 08:50:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+
+const mongoose = require("mongoose");
+const express = require("express");
+const studentRoute = require("./controller/studentRoute");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+
+// MongoDB Atlas connection
+mongoose.set("strictQuery", true);
+mongoose.connect("mongodb+srv://pratikjodpj:12345@cluster0.pzf8jhz.mongodb.net/schooldb");
+var db = mongoose.connection;
+db.on("open", () => console.log("Connected to DB"));
+db.on("error", () => console.log("Error Occurred"));
+
+//Creating app
+const app = express();
+
+//Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
+app.use("/students",studentRoute);
+
+// Listening to port number
+app.listen(4000, () => {
+    console.log("Server started at 4000");
+})
